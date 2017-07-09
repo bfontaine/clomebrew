ea536d2ff6b065778144858c0a15a8e1eaada686
Baptiste Fontaine <b@ptistefontaine.fr>
2017-07-09 22:47:04 +0200

    namespace fix

--- test/clomebrew/core_test.clj
+++ test/clomebrew/core_test.clj
@@ -1,7 +1,3 @@
-(ns clojure-brew.core-test
+(ns clomebrew.core-test
   (:require [clojure.test :refer :all]
-            [clojure-brew.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+            [clomebrew.core :refer :all]))
