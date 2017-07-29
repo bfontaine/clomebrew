7e49770a3b95d47ec39b3bd2974634856fd10554
Baptiste Fontaine <b@ptistefontaine.fr>
2017-07-29 15:10:40 +0200

    Add core tests

--- test/clomebrew/core_test.clj
+++ test/clomebrew/core_test.clj
@@ -1,3 +1,13 @@
 (ns clomebrew.core-test
   (:require [clojure.test :refer :all]
             [clomebrew.core :as hb]))
+
+(deftest new-brew
+  (is (some? (hb/new-brew))))
+
+(deftest paths
+  (let [b (hb/new-brew)]
+    (is (string? (hb/cache b)))
+    (is (string? (hb/cellar b)))
+    (is (string? (hb/prefix b)))
+    (is (string? (hb/repository b)))))
